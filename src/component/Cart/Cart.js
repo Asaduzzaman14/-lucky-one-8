dd27f200bf19222c8252fb33f498b317167db163
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-26 10:43:56 +0600

    sticky cart

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,10 +5,13 @@ import { MdDelete } from 'react-icons/md';
 const Cart = ({ item }) => {
     // console.log(item);
     return (
-        <div className='cart-item'>
-            <img src={item.img} alt="" />
-            <h3>{item.name}</h3>
-            <MdDelete className='icon'></MdDelete>
+        <div className='cart'>
+            <div className='cart-item'>
+                <img src={item.img} alt="" />
+                <h3>{item.name}</h3>
+                <MdDelete className='icon'></MdDelete>
+
+            </div>
 
         </div>
     );
